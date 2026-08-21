82e37c48d13fb4cad61687a66cfe28953c82d31b
rayaabumunshar <rayaabumunshar2005@gmail.com>
2026-08-21 19:31:01 +0300

    Add quote API and initial CI workflow

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.get("/api/quote", async (req, res) => {
+  try {
+    const response = await fetch("https://dummyjson.com/quotes/random");
+    if (!response.ok) {
+      throw new Error("Failed to fetch quote");
+    }
+    const data = await response.json();
+    res.json({
+      quote: data.quote,
+      author: data.author,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Could not get a quote.",
+    });
+  }
+});
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
